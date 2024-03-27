310046bca4111082e12409bc799e223ce9d3ff9e
XiaoBaiXueHua <flaminngpancakes@gmail.com>
2024-03-27 12:08:05 -0700

    mar 27 lab prep

---
+++ mar27/mar27/mar27.cpp
@@ -0,0 +1,16 @@
+// mar27.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <array>
+#include <algorithm> //for sorting and whatnot from <array>
+#include "subfiles/mar27.h" //change this if the .h file name is changed
+#include "subfiles/mar27functions.cpp" //change this if the functions file name is changed
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
