0f247c078292466bef86b8b81b194bfbdbab3f22
stevenyonduan <165136717+stevenyonduan@users.noreply.github.com>
2024-04-08 13:13:04 -0700

    Add files via upload

--- CurrencyExchange/CurrencyExchange/subfiles/exchange.cpp
+++ CurrencyExchange/CurrencyExchange/subfiles/exchange.cpp
@@ -1,1 +1,18 @@
-//function file
+//
+//  exchange.cpp
+//  exchange
+//
+//  Created by Steven Yon on 4/8/24.
+//
+
+#include "exchange.h"
+#include <iostream>
+#include <iomanip>
+
+void title(){
+    std::cout << std::setw(38) << "*****************" << std::endl;
+    std::cout << std::setw(31) << "Welcome to Currency Exchange" << std::endl;
+    std::cout << std::setw(38) << "*****************" << std::endl;
+}
+
+
--- CurrencyExchange/CurrencyExchange/subfiles/exchange.h
+++ CurrencyExchange/CurrencyExchange/subfiles/exchange.h
@@ -1,1 +1,23 @@
-//header file
+//
+//  exchange.h
+//  exchange
+//
+//  Created by Steven Yon on 4/8/24.
+//
+
+#ifndef exchange_h
+#define exchange_h
+
+
+class exchange {
+public:
+    
+    void title();
+    
+    
+    
+private:
+    
+};
+
+#endif /* exchange_h */
