1ad8605f16a57480013a9d64e350e10570aecf88
ThiagoHilay <thiagohilay1999@gmail.com>
2024-04-08 13:41:46 -0700

    Update exchange.cpp
    vector fixing

--- CurrencyExchange/CurrencyExchange/subfiles/exchange.cpp
+++ CurrencyExchange/CurrencyExchange/subfiles/exchange.cpp
@@ -4,12 +4,11 @@
 #include<string>
 #include<iomanip>
 #include<locale>
-
+#include <stdexcept>
 
 using namespace std;
-void vecInit(std::vector<vector<double>>& vec) { //vector vecInit with 10 items, initialized to the integers values
-	vec =
-	{ 862.3, 0.00116 },
+vector<vector<double>> vecInit{ //vector vecInit with 10 items, initialized to the integers values
+	{862.3, 0.00116},
 	{ 5.06, 0.198 },
 	{ 1.36, 0.74 },
 	{ 7.23, 0.14 },
@@ -20,9 +19,9 @@ void vecInit(std::vector<vector<double>>& vec) { //vector vecInit with 10 items,
 	{ 16.48, 0.061 },
 	{ 92.38, 0.011 }
 };
-vector <string> cntyInit(10) { //vector cntyInit string, with 10 countries 
-	Argentina Peso Brazilian Real, Canadian Dollar, Chinese Yuan, Euro, Hong Kong Dollar, Indian Rupee,
-		Japanese Yen, Mexican Peso, Russian Ruble;
+vector <string> cntyInit{ //vector cntyInit string, with 10 countries 
+	"Argentina Peso", "Brazilian Real", "Canadian Dollar", "Chinese Yuan", "Euro", "Hong Kong Dollar", "Indian Rupee",
+		"Japanese Yen", "Mexican Peso", "Russian Ruble"
 };
 void outputVector() { //output the countries with the currencies in form of list
 	for (int country : cntyInit) { //for loop for showing the countries 
@@ -36,5 +35,16 @@ void outputVector() { //output the countries with the currencies in form of list
 }
 
 int main() {
-	output vector();
+	vector<vector<double>> vec{ //vector vecInit with 10 items, initialized to the integers values
+		{ 862.3, 0.00116 },
+		{ 5.06, 0.198 },
+		{ 1.36, 0.74 },
+		{ 7.23, 0.14 },
+		{ 0.92, 1.08 },
+		{ 7.83, 0.13 },
+		{ 83.25, 0.012 },
+		{ 151.8, 0.0066 },
+		{ 16.48, 0.061 },
+		{ 92.38, 0.011 }
+	};
 }
