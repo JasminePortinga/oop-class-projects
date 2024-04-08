5f6e368d1cb531f3072d881d470221065eb86bcd
stevenyonduan <165136717+stevenyonduan@users.noreply.github.com>
2024-04-08 14:20:41 -0700

    Update CurrencyExchange.cpp

--- CurrencyExchange/CurrencyExchange/CurrencyExchange.cpp
+++ CurrencyExchange/CurrencyExchange/CurrencyExchange.cpp
@@ -25,3 +25,47 @@ void greeting() {
     cout << setw(50) << "*\tWelcome to Currency Exchange\t*" << endl;
     cout << setw(50) << stars << endl;
 }
+
+ char ans1{'Y'};
+    int ans2{0};
+    
+    while (toupper(ans1) == 'Y'){
+    std::cout << "Enter 1 to convert from US dollar to other currency. 2 to convert from other currency to US dollar: ";
+    std::cin >> ans2;
+    
+        double i{0}, amt{0};
+        
+        if (ans2==1){
+            std::cout << "First choose a country: ";
+            std::cin >> i;
+            std::cout << std::endl;
+            std::cout << "Enter the amount in US Dollars: ";
+            std::cin >> amt;
+            std::cout << std::endl;
+            std::cout << "From US to " << /* Vector string */ "\t$" << amt << "\t" << amt /* Vector value */ << std::endl;
+            
+        }
+        
+        else if (ans2==2){
+            
+            std::cout << "First choose a country: ";
+            std::cin >> i;
+            std::cout << std::endl;
+            std::cout << "Enter the amount in foreign currency: ";
+            std::cin >> amt;
+            std::cout << std::endl;
+            std::cout << "From to US Dollars:\t" << amt << "\t$" << amt /*Vector value*/ << std::endl;
+            
+        }
+        
+        else {
+            
+        }
+        
+        std::cout << "Enter y/Y to continue, else enter anything: ";
+        std::cin >> ans1;
+    }
+    
+
+    
+    return 0;
