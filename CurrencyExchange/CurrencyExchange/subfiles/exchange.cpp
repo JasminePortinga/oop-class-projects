14bcfef2d17f0a5fdb5898c60f2ffd5d2e166db0
JasminePortinga <portinga.jasmine@gmail.com>
2024-04-10 11:59:37 -0700

    skeleton rewrite of currency exchange

--- CurrencyExchange/CurrencyExchange/subfiles/exchange.cpp
+++ CurrencyExchange/CurrencyExchange/subfiles/exchange.cpp
@@ -1,108 +1,63 @@
 // function file
+#include "./exchange.h"
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
-#include <iomanip>
-#include <locale>
-#include <stdexcept>
 
-using namespace std;
-
-int vec_size{10}; // initialize to 10
-
-vector<vector<double>> vecInit{
-    // vector vecInit with 10 items, initialized to the integers values
-    {862.3, 0.00116}, // peso
-    {5.06, 0.198},    // brazilian real
-    {1.36, 0.74},     // canadian dollar
-    {7.23, 0.14},     // cn yuan
-    {0.92, 1.08},     // euro
-    {7.83, 0.13},     // hk dollar
-    {83.25, 0.012},   // indian rupee
-    {151.8, 0.0066},  // jpy
-    {16.48, 0.061},   // mexican peso
-    {92.38, 0.011}    // russian ruble
-};
-vector<string> cntyInit{// vector cntyInit string, with 10 countries
-                        "Argentina Peso", "Brazilian Real", "Canadian Dollar", "Chinese Yuan", "Euro", "Hong Kong Dollar", "Indian Rupee",
-                        "Japanese Yen", "Mexican Peso", "Russian Ruble"};
-void outputVector(int user_amt)
-{ // output the countries with the currencies in form of list
-
-        cout << setw(35) << " " << setw(10) << "$1" << setw(15) << std::string{"$" + to_string(user_amt)} << endl;
-    for (int i{0}; i < vec_size; i++)
-    {
-        double to_foreign{vecInit[i][0]};
-        cout << setw(35) << cntyInit[i] << setw(10) << to_foreign << setw(15) << to_foreign * user_amt << endl;
-    }
-    /*for (double &currency : vecInit) { //for loop for showing the countries
-        //cout << country + 1 << "  ";
-        //for (int currency : vecInit) { //output the currency values by side of the countries
-        for (std::string country : cntyInit) {
-            //cout << currency + 1 << "  ";
-        }
-        cout << endl; //one line for each country with the currency
-    }*/
-    cout << endl;
-}
-
-class country
+/* country class */
+// constructor w/country name n two exchange rates
+country::country(std::string n, std::string c, double t_f, double t_u)
 {
-    country(std::string name, double to_foreign, double rate);
+	this->setName(n);
+	this->setCurrency(c);
+	this->setForeignRate(t_f);
+	this->setUSDRate(t_u);
+}
 
-public:
-    int user_index(int i)
-    {
-        return i + 1;
-    }
-
-private:
-};
-
-int main()
+// set functions
+void country::setForeignRate(double t_f)
 {
-
-    //outputVector(100);
-
-    /*
-    vector<vector<double>> vec{ //vector vecInit with 10 items, initialized to the integers values
-        { 862.3, 0.00116 },
-        { 5.06, 0.198 },
-        { 1.36, 0.74 },
-        { 7.23, 0.14 },
-        { 0.92, 1.08 },
-        { 7.83, 0.13 },
-        { 83.25, 0.012 },
-        { 151.8, 0.0066 },
-        { 16.48, 0.061 },
-        { 92.38, 0.011 }
-    };*/
 }
+void country::setUSDRate(double t_u) {}
+void country::setName(std::string n) {}
+void country::setCurrency(std::string c) {}
 
-//for loops from steven:
-int main() {
-   exchange title;
-    
-    
-    char ans1{'Y'};
-    int ans2{0};
-    
-    do{
-    std::cout << "Enter y/Y to continue, else enter anything: ";
-    std::cin >> ans1;
-    std::cout << std::endl;
-    std::cout << "Enter 1 to convert from US dollar to other currency. 2 to convert from other currency to US dollar: ";
-    std::cin >> ans2;
-    
-        if (ans2==1){
-            
-        }
-        
-        else{
-            
-        }
-    }
-    while (toupper(ans1) == 'Y');
-    
-    return 0;
+// get functions
+double country::getFR() {}
+double country::getUSR() {}
+std::string country::getName() {}
+std::string country::getCurrency() {}
+
+/* exchange class */
+exchange::exchange() {
+	this->cntyInit();
 }
+// put all the cities into a vector. hard-coded is okay
+void exchange::cntyInit()
+{
+	addCtry("Argentina","Argentinian Peso", 862.3, 0.0016);
+	addCtry("Brazil", "Brazilian Real", 5.06, 0.198);
+	//etc
+}
+// made a function for adding countries to the vector it bc i'm lazy and also it will be necessary for later
+void exchange::addCtry(std::string n, std::string c, double t_f, double t_u)
+{										// adding object instances into a vector: https://stackoverflow.com/questions/15802006/how-can-i-create-objects-while-adding-them-into-a-vector
+	ctries.push_back(country(n, c, t_f, t_u)); 
+}
+//bc the looped version of the conversion display is different from the initial one, greeting() should include the unique conversion display
+void exchange::greeting() {}
+//y'know, like a bank teller window lol
+void exchange::window() {
+	//first ask whether the user would like to convert to foreign currency or to usd. save this as a variable
+	//then, ask how much they'd like to convert. this number should be either a float or a double, unless you want to specify "whole dollars only"
+	//then, display all the available countries n their conversion rates in their neat three columns. make sure they're numbered for the user to call on them! this should be a loop going through the ctries vector for the relevant info
+	//then ask the user which country they'd like to convert the currency to, input as an integer. if they enter a number greater than 12, return an error and ask them to reenter a number
+	//take that integer and use it to locate that country's object instance in the vector
+	//call upon the relevant function for conversion 
+	//no need to ask if they'd like to continue, as that's in the main loop
+}
+//these conversion functions take integers bc those integers will be the index of the country we're referencing in the vector
+//bc they are void f'ns, they should also have a cout to display the final numbers
+void exchange::convForeign(int i) {}
+void exchange::convUSD(int i) {}
--- CurrencyExchange/CurrencyExchange/subfiles/exchange.h
+++ CurrencyExchange/CurrencyExchange/subfiles/exchange.h
@@ -1,23 +1,41 @@
-//
-//  exchange.h
-//  exchange
-//
-//  Created by Steven Yon on 4/8/24.
-//
+//header file
+#ifndef CURRENCY_EXCHANGE
+#define CURRENCY_EXCHANGE
 
-#ifndef exchange_h
-#define exchange_h
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
 
-
-class exchange {
+class country
+{
 public:
-    
-    void title();
-    
-    
-    
+	country(){}; // default constructor
+	country(std::string, std::string, double, double);
+	 // constructor w/country name n the two exchange rates
+	void setForeignRate(double), setUSDRate(double), setName(std::string), setCurrency(std::string);
+	double getFR(), getUSR(); //shorthanded these fn names bc they will probably be used a fair bit in the exchange class for the actual exchange rate
+	std::string getName();
+	std::string getCurrency();
+
 private:
-    
+	double to_foreign, to_usd; // vars for the exchange rates to the foreign currency n to usd
+	std::string name, currency;		   // country & currency name
 };
 
-#endif /* exchange_h */
+class exchange
+{
+public:
+	exchange(); //default constructor
+	void cntyInit();
+	void addCtry(std::string, std::string, double, double);
+	void greeting();
+	void window();
+	void convForeign(int);
+	void convUSD(int);
+
+private:
+	// make the vector of exchanges out of country objects
+	std::vector<country> ctries{};
+};
+#endif
